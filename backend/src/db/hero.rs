b517152862cfbe4968fae9c9f73abce52b34a226
kyunghyunHan <ekfqlwcjswl@gmail.com>
2022-09-26 11:48:59 +0900

    add

--- backend/src/db/hero.rs
+++ backend/src/db/hero.rs
@@ -1,5 +1,29 @@
+use crate::db::connecion::Conn;
 use crate::db::schema::heroes;
+use diesel::prelude::*;
+// pub fn all(connection: Conn) -> QueryResult<Vec<Hero>> {
+//     heroes::table.load::<Hero>(*connection)
+// }
 
+// pub fn get(id: i32, connection: &Conn) -> QueryResult<Hero> {
+//     heroes::table.find(id).get_result::<Hero>(connection)
+// }
+
+// pub fn insert(person: Hero, connection: Conn) -> QueryResult<Hero> {
+//     diesel::insert_into(heroes::table)
+//         .values(&InsertableHero::from_person(person))
+//         .get_result(&*connection)
+// }
+
+// pub fn update(id: i32, person: Hero, conn: Conn) -> QueryResult<Hero> {
+//     diesel::update(heroes::table.find(id))
+//         .set(&person)
+//         .get_result(connection)
+// }
+
+// pub fn delete(id: i32, connection: &Conn) -> QueryResult<usize> {
+//     diesel::delete(heroes::table.find(id)).execute(connection)
+// }
 #[table_name = "heroes"]
 #[derive(Serialize, Deserialize, Queryable, Insertable)]
 pub struct Hero {
@@ -11,11 +35,14 @@ pub struct Hero {
 }
 /// This represents a page view being inserted into the database, without the auto-generated fields
 
-#[derive(Deserialize, Insertable)]
-#[table_name = "heroes"]
-pub struct InsertableHero {
-    pub name: String,
-    pub identity: String,
-    pub hometown: String,
-    pub age: i32,
+impl Hero {
+    fn from_person(hero: Hero) -> Hero {
+        Hero {
+            id: hero.id,
+            name: hero.name,
+            identity: hero.identity,
+            hometown: hero.hometown,
+            age: hero.age,
+        }
+    }
 }
